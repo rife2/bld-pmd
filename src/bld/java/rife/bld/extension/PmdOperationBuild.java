a2d37055c6f3b6321dd6b41cf4705df7d0a09524
Erik C. Thauvin <erik@thauvin.net>
2025-02-24 23:24:32 -0800

    Bump JUnit to version 5.12.0

--- src/bld/java/rife/bld/extension/PmdOperationBuild.java
+++ src/bld/java/rife/bld/extension/PmdOperationBuild.java
@@ -47,8 +47,8 @@ public class PmdOperationBuild extends Project {
         scope(runtime)
                 .include(dependency("org.slf4j", "slf4j-simple", version(2, 0, 16)));
         scope(test)
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 0)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
