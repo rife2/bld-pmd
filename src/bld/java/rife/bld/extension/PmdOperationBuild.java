5d55bda0036094534ee4a77cf4c763f5f936f8a5
Erik C. Thauvin <erik@thauvin.net>
2025-06-27 07:05:41 -0700

    Version 1.3.1

--- src/bld/java/rife/bld/extension/PmdOperationBuild.java
+++ src/bld/java/rife/bld/extension/PmdOperationBuild.java
@@ -34,7 +34,7 @@ public class PmdOperationBuild extends Project {
     public PmdOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-pmd";
-        version = version(1, 3, 0);
+        version = version(1, 3, 1);
 
         javaRelease = 17;
 
