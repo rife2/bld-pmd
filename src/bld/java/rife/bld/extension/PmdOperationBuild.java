0a2dc0592d759508e304fb40aee16df51c77fb50
Erik C. Thauvin <erik@thauvin.net>
2025-12-30 11:08:56 -0800

    Bump PMD to version 7.20.0

--- src/bld/java/rife/bld/extension/PmdOperationBuild.java
+++ src/bld/java/rife/bld/extension/PmdOperationBuild.java
@@ -42,7 +42,7 @@ public class PmdOperationBuild extends Project {
 
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
-        var pmd = version(7, 19, 0);
+        var pmd = version(7, 20, 0);
         var junit = version(6, 0, 1);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld",
