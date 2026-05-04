99e15f15c7f97875c5083ef4954ab0f16fb8ccb6
Erik C. Thauvin <erik@thauvin.net>
2026-05-03 23:04:50 -0700

    Version 1.6.5-SNAPSHOT

--- src/bld/java/rife/bld/extension/PmdOperationBuild.java
+++ src/bld/java/rife/bld/extension/PmdOperationBuild.java
@@ -34,7 +34,7 @@ public class PmdOperationBuild extends Project {
     public PmdOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-pmd";
-        version = version(1, 6, 0);
+        version = version(1, 6, 5, "SNAPSHOT");
 
         javaRelease = 17;
 
@@ -62,7 +62,7 @@ public class PmdOperationBuild extends Project {
                         version(2, 0, 17)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
-                        version(1, 0, 0)))
+                        version(1, 0, 1)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
                 .include(dependency("org.assertj", "assertj-core",
