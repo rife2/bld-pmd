9b8a659ed1cde320549ac9d59ec08b46cda1e308
Erik C. Thauvin <erik@thauvin.net>
2026-09-04 12:06:23 -0700

    Bump SLF4J to version 2.0.19

--- src/bld/java/rife/bld/extension/PmdOperationBuild.java
+++ src/bld/java/rife/bld/extension/PmdOperationBuild.java
@@ -60,7 +60,7 @@ public class PmdOperationBuild extends Project {
         scope(runtime)
                 .include(dependency(
                         "org.slf4j", "slf4j-simple",
-                        version(2, 0, 18)));
+                        version(2, 0, 19)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-testing-helpers",
                         version(1, 1, 0, "SNAPSHOT")))
