5e125c43454d6c222110d61ffe6467e00cd762ed
Erik C. Thauvin <erik@thauvin.net>
2026-09-12 20:10:17 -0700

    Bump Testing Helpers to version 1.1.1

--- src/bld/java/rife/bld/extension/PmdOperationBuild.java
+++ src/bld/java/rife/bld/extension/PmdOperationBuild.java
@@ -34,7 +34,7 @@ public class PmdOperationBuild extends Project {
     public PmdOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-pmd";
-        version = version(1, 7, 0, "SNAPSHOT");
+        version = version(1, 7, 0);
 
         javaRelease = 17;
 
@@ -62,7 +62,7 @@ public class PmdOperationBuild extends Project {
                         version(2, 0, 19)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-testing-helpers",
-                        version(1, 1, 0)))
+                        version(1, 1, 1)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
                 .include(dependency("org.assertj", "assertj-core",
